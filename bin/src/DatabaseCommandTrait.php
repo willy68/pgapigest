7217c247b4f5bd60f638e771f24c8a14eaf1ac88
William Lety <william.lety@gmail.com>
2020-03-26 15:30:40 +0100

    Test Foreign Key

--- bin/src/DatabaseCommandTrait.php
+++ bin/src/DatabaseCommandTrait.php
@@ -45,7 +45,43 @@ trait DatabaseCommandTrait
       WHERE TABLE_SCHEMA = '{$db}' AND TABLE_NAME = '{$table}'
       AND COLUMN_NAME NOT IN ('id','created_at','updated_at','password')";
     }
-  
+
+    /**
+     * 
+     *
+     * @param string $db
+     * @param string $table
+     * @return string
+     */
+    protected function getForeignKeyQuery(string $db, string $table): string
+    {
+        return "SELECT i.TABLE_NAME, k.REFERENCED_TABLE_NAME, k.REFERENCED_COLUMN_NAME 
+        FROM information_schema.TABLE_CONSTRAINTS i 
+        LEFT JOIN information_schema.KEY_COLUMN_USAGE k
+        ON i.CONSTRAINT_NAME = k.CONSTRAINT_NAME 
+        WHERE i.CONSTRAINT_TYPE = 'FOREIGN KEY' 
+        AND i.TABLE_SCHEMA = '{$db}'
+        AND i.TABLE_NAME = '{$table}'";
+    }
+
+    /**
+     * 
+     *
+     * @param string $db
+     * @param string $table
+     * @return string
+     */
+    protected function getChildRelationQuery(string $db, string $table): string
+    {
+        return "SELECT 
+        TABLE_NAME, COLUMN_NAME, REFERENCED_TABLE_NAME
+      FROM
+        INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+      WHERE
+        REFERENCED_TABLE_SCHEMA = '{$db}' AND
+        REFERENCED_TABLE_NAME = '{$table}'";
+    }
+
     /**
      *
      *
@@ -55,13 +91,35 @@ trait DatabaseCommandTrait
     protected function getColumnsArray(string $sql): array
     {
         $columns = $this->getColumns($sql);
-        $cols = array();
+        $cols = [];
         while ($column = $columns->fetch(\PDO::FETCH_ASSOC)) {
             $cols[] = $column['COLUMN_NAME'];
         }
         return $cols;
     }
 
+    /**
+     * get unnamed columns
+     *
+     * @param string $sql
+     * @return array
+     */
+    protected function getFetchColumns(string $sql): array
+    {
+        $columns = $this->getColumns($sql);
+        $cols = [];
+        while ($column = $columns->fetch(\PDO::FETCH_ASSOC)) {
+            $cols[] = $column;
+        }
+        return $cols;
+    }
+
+    /**
+     * 
+     *
+     * @param \Psr\Container\ContainerInterface $c
+     * @return \PDO
+     */
     protected function getDao(ContainerInterface $c): \PDO
     {
         if (!$this->dao) {
